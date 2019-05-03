567e8fafa13c5ff8bdae0e152db316e6c11cded2
vincent <ododovincent54@gmail.com>
2019-05-03 16:52:30 +0300

    added jQuery code to make page responsive

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+    $("#design").click(function() {
+      $("#Our").show();
+      $("#design").hide();
+     
+        $("#Our").click(function(){
+          $("#design").show();
+      $("#Our").hide();
+
+        });
+
+      });
+      
+    });
+$(document).ready(function() {
+ $("#dev").click(function() {
+      $("#All").show();
+      $("#dev").hide();
+     
+        $("#All").click(function(){
+          $("#dev").show();
+      $("#All").hide();
+
+        });
+
+      });
+    });
+      
+    $(document).ready(function() {
+      $("#product").click(function() {
+           $("#and").show();
+           $("#product").hide();
+          
+             $("#and").click(function(){
+               $("#product").show();
+           $("#and").hide();
+     
+             });
+     
+           });
+         });
+           
+  
